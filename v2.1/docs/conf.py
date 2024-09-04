777ab657f41561cef628368fcbec1fca438381db
Antonio Olleros <antonio.olleros@meaningfuldata.eu>
2024-09-04 11:51:42 +0200

    deletes duplicated folders

--- v2.1/docs/conf.py
+++ v2.1/docs/conf.py
@@ -74,7 +74,7 @@ for op_type in next(os.walk("reference_manual/operators"))[1]:
                     "examples": examples,
                     "inputs": inputs,
                     "op_type": op_type,
-                    "repourl_ex": f"https://github.com/sdmx-twg/vtl/blob/{BRANCH}/v{VERSION}/docs/",
+                    "repourl_ex": f"https://github.com/sdmx-twg/vtl/blob/{BRANCH}/v{VERSION}/docs",
                 }
             )
         if examples_folder.joinpath("end_text.rst").exists():
