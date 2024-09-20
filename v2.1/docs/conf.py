8a8cfcd0f72fbdd673bd3c01fb259a423eca51ed
Antonio Olleros <antonio.olleros@meaningfuldata.eu>
2024-09-20 18:28:52 +0200

    Fix PDF generation

--- v2.1/docs/conf.py
+++ v2.1/docs/conf.py
@@ -13,15 +13,21 @@ def name_norm(value):
 
 
 html_theme = "sphinx_rtd_theme"
+
+
 extensions = [
     "sphinxcontrib.mermaid",
     "sphinxcontrib.plantuml",
     "rst2pdf.pdfbuilder",
     "sphinx_toolbox.collapse"
 ]
+
+
 pdf_documents = [
     ("index", f"VTL_{VERSION}_DOCS", f"VTL {VERSION} DOCS", "SDMX-TWG"),
 ]
+
+
 exclude_patterns = [
     "*intro.rst",
     "pandocTranslation*",]
@@ -31,6 +37,8 @@ html_theme_options = {
     "navigation_depth": 5,
     "collapse_navigation": False,
 }
+
+
 html_context = {
     "display_github": True,  # Integrate GitHub
     "github_user": "amattioc",  # Username
@@ -50,29 +58,29 @@ for template in next(os.walk("templates"))[2]:
 # Apply templates in each op type folder
 for op_type in next(os.walk("reference_manual/operators"))[1]:
     op_type_path = Path("reference_manual/operators").joinpath(op_type)
+
+
     # Apply templates in each op folder
     for op_folder in next(os.walk(op_type_path))[1]:
         op_path = op_type_path.joinpath(op_folder)
-        # # Write the op main page
-        # with open(op_path.joinpath("index.rst"), "w") as f:
-        #     f.write(templates["operator"].render({"title": op_folder}))
 
         # Write the op examples
         examples_folder = op_path.joinpath("examples")
-
         ds_list = sorted(
             x.with_suffix("").name for x in examples_folder.glob("ds_*.csv")
         )
         inputs = []
         for i in range(len(ds_list)):
-            inputs.append({"folder": examples_folder, "i": i + 1, "name": ds_list[i]})
+            inputs.append(
+                {"folder": examples_folder, "i": i + 1, "name": ds_list[i]})
 
         ex_list = sorted(
             x.with_suffix("").name for x in examples_folder.glob("ex_*.vtl")
         )
         examples = []
         for i in range(len(ex_list)):
-            examples.append({"folder": examples_folder, "i": i + 1, "name": ex_list[i]})
+            examples.append(
+                {"folder": examples_folder, "i": i + 1, "name": ex_list[i]})
 
         examples_text = templates["examples"].render(
                 {
@@ -84,6 +92,7 @@ for op_type in next(os.walk("reference_manual/operators"))[1]:
             )
         if examples_folder.joinpath("end_text.rst").exists():
             examples_text += """.. include:: examples/end_text.rst"""
+
         with open(op_path.joinpath("examples.rst"), "w") as f:
             f.write(examples_text)
 
--- v2.1/docs/reference_manual/overview_conventions/vtl_ml_operators.rst
+++ v2.1/docs/reference_manual/overview_conventions/vtl_ml_operators.rst
@@ -3,3 +3,5 @@ VTL-ML Operators
 
 .. csv-table::
    :file: ./operatorstable.csv
+   :widths: 30, 70
+   :header-rows: 1
